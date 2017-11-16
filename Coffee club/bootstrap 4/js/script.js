4bd3aa4bbb25c1e2b0ba4ef274b6e0bfa3dcb442
Jakub Ławniczak <lapa19904@hotmail.com>
2017-11-16 14:23:14 +0100

    1.01
    Firs upload

---
+++ Coffee club/bootstrap 4/js/script.js
@@ -0,0 +1,12 @@
+$('body').scrollspy({
+            target: '#myNav'
+        })
+        $('#myNav ul li a[href^="#"]').on('click', function(e) {
+            e.preventDefault();
+            var t = this.hash;
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top
+            }, 1000, function () {
+                window.location.hash = t;
+            });
+        });
